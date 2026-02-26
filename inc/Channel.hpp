3c047264bd1dc0c1f1343d0acf973a7ae5c6aa9a
mingde <myuen@student.42singapore.sg>
2026-02-26 22:29:55 +0800

    PART

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Channel.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jngew <jngew@student.42singapore.sg>       +#+  +:+       +#+        */
+/*   By: myuen <myuen@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 19:56:36 by jngew             #+#    #+#             */
-/*   Updated: 2026/02/26 21:31:14 by jngew            ###   ########.fr       */
+/*   Updated: 2026/02/26 22:31:48 by myuen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,33 +19,36 @@
 
 class	Channel
 {
-	public:
-				Channel(std::string name);
-				~Channel();
+public:
+	Channel(std::string name);
+	~Channel();
 
-				std::string	getName() const;
+	std::string	getName() const;
 
-				void		addMember(Client *client);
-				void		removeMember(Client *client);
-				bool		isMember(Client *client);
-				void		addOperator(Client *client);
-				void		removeOperator(Client *client);
-				bool		isOperator(Client *client);
-				bool		isEmpty() const;
-				std::string	getUserList();
+	void		addMember(Client *client);
+	void		removeMember(Client *client);
+	bool		isMember(Client *client);
+	void		addOperator(Client *client);
+	void		removeOperator(Client *client);
+	bool		isOperator(Client *client);
+	bool		isEmpty() const;
+	std::string	getUserList();
+	bool 		hasOperators() const;
+	Client* 	getFirstMember() const;
+	
+	void		broadcast(std::string message, Client *exclude);
 
-				void	broadcast(std::string message, Client *exclude);
-	private:
-				std::string				_name;
-				std::vector<Client *>	_members;
-				std::vector<Client *>	_operators;
+private:
+	std::string				_name;
+	std::vector<Client *>	_members;
+	std::vector<Client *>	_operators;
 
-				std::string _topic;              	// For TOPIC command
-        		std::string _password;           	// For MODE +k
-        		bool _inviteOnly;                	// For MODE +i
-        		bool _topicRestricted;           	// For MODE +t
-        		int _userLimit;                  	// For MODE +l (-1 = no limit)
-        		std::vector<Client *> _inviteList;  // For INVITE command
+	std::string				_topic;             // For TOPIC command
+	std::string				_password;          // For MODE +k
+	bool					_inviteOnly;        // For MODE +i
+	bool					_topicRestricted;   // For MODE +t
+	int						_userLimit;         // For MODE +l (-1 = no limit)
+	std::vector<Client *>	_inviteList;		// For INVITE command
 };
 
 #endif
